32493eb21eb200cc40a92c06e9ba944a75731cf6
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2024-12-10 13:41:15 -0300

    Vashjir Enforcer and Infernaling pets

--- data/sql/db-world/updates/smartstone.sql
+++ data/sql/db-world/updates/smartstone.sql
@@ -122,6 +122,42 @@ INSERT INTO `smartstone_pets` (`CreatureId`, `Enabled`, `Description`) VALUES
 (@Entry, 1, @Description);
 
 SET
+@Entry := 80004,
+@Name := "Vashj'ir Enforcer",
+@Model := 20200,
+@Scale := 0.2,
+@Description := "Summon Vashj'ir Enforcer";
+
+DELETE FROM `creature_template` WHERE `entry` = @Entry;
+INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
+(@Entry, @Name, NULL, NULL, 0, 1, 1, 2, 35, 0, @Scale, 0, 0, 2000, 0, 1, 0, 7, 0, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, '');
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = @Entry;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(@Entry, 0, @Model, @Scale, 1, 0);
+ 
+INSERT INTO `smartstone_pets` (`CreatureId`, `Enabled`, `Description`) VALUES
+(@Entry, 1, @Description);
+
+SET
+@Entry := 80005,
+@Name := "Infernaling",
+@Model := 12817,
+@Scale := 0.2,
+@Description := "Summon Vashj'ir Enforcer";
+
+DELETE FROM `creature_template` WHERE `entry` = @Entry;
+INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
+(@Entry, @Name, NULL, NULL, 0, 1, 1, 2, 35, 0, @Scale, 0, 0, 2000, 0, 1, 0, 7, 0, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, '');
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = @Entry;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(@Entry, 0, @Model, @Scale, 1, 0);
+ 
+INSERT INTO `smartstone_pets` (`CreatureId`, `Enabled`, `Description`) VALUES
+(@Entry, 1, @Description);
+
+SET
 @Entry := 90001,
 @Name := "Winterveil Helper",
 @Model := 19342,
