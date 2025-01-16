a9b979f86f68d03163cb267ff8ee7134d8d9aa09
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2025-01-15 23:02:56 -0300

    Some more flavour

--- data/sql/db-world/updates/smartstone.sql
+++ data/sql/db-world/updates/smartstone.sql
@@ -183,10 +183,10 @@ INSERT INTO `smartstone_pets` (`CreatureId`, `Enabled`, `Description`) VALUES
 -- Thunderwing AI
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 80006;
 
-DELETE FROM `smart_scripts` WHERE (`entryorguid` = 80006) AND (`source_type` = 0);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryorguid` = 80006);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param6`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(80006, 0, 0, 0, 1, 0, 100, 1, 1, 1, 0, 0, 0, 0, 11, 37964, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Thunderwing - Out of Combat - Cast \'Call Lightning (Cosmetic)\' (No Repeat)');
-
+(80006, 0, 0, 0, 1, 0, 100, 1, 1, 1, 0, 0, 0, 0, 11, 37964, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Thunderwing - Out of Combat - Cast \'Call Lightning (Cosmetic)\' (No Repeat)'),
+(80006, 0, 1, 0, 1, 0, 100, 0, 3000, 6000, 30000, 60000, 0, 0, 75, 32910, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Thunderwing - Out of Combat - Add Aura \'Windfury\'');
 SET
 @Entry := 90001,
 @Name := "Winterveil Helper",
