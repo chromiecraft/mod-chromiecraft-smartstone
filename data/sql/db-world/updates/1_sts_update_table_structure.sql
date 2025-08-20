c5f48e61b8dd16c02d7abda5d0b4f8b37bf856a2
Ryan Turner <AugustoMendes4426@gmail.com>
2025-08-20 12:57:50 +0100

    Renamed sts update to stats update table structure
    Also added SCALE change to this.

--- data/sql/db-world/updates/1_sts_update_table_structure.sql
+++ data/sql/db-world/updates/1_sts_update_table_structure.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `smartstone_categories` RENAME COLUMN `CategoryType` TO `ParentCategory`;
 ALTER TABLE `smartstone_services` ADD COLUMN `Category` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ServiceId`;
 ALTER TABLE `smartstone_pets` ADD COLUMN `Type` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `CreatureId`;
+ALTER TABLE `smartstone_costumes` ADD COLUMN `Scale` FLOAT NOT NULL DEFAULT 1 AFTER `DisplayId`;
