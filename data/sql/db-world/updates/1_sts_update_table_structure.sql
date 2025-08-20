3efa2b9d360728949aed32c2081fd02c26cdb5fe
Ryan Turner <AugustoMendes4426@gmail.com>
2025-08-20 16:07:33 +0100

    Update 1_sts_update_table_structure.sql

--- data/sql/db-world/updates/1_sts_update_table_structure.sql
+++ data/sql/db-world/updates/1_sts_update_table_structure.sql
@@ -1,17 +1,45 @@
--- ADD always "IF NOT EXISTS" between ADD COLUMN and Table name for the cases of adding to a table. 
 -- Use the example how to prevent errors in the cases that commands DO NOT support "IF NOT EXISTS" by usage of procedures.
 
 DELIMITER // 
 CREATE PROCEDURE RenameCategoryTypeIfExists() 
-BEGIN -- Checks if CategoryType in smartstone_categories exists, before renaming, if doesn't exist it just skips this.
+BEGIN -- ALTER TABLE `smartstone_categories` RENAME COLUMN `CategoryType` TO `ParentCategory`;
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'smartstone_categories' AND COLUMN_NAME = 'CategoryType') THEN
-        ALTER TABLE `smartstone_categories` RENAME COLUMN `CategoryType` TO `ParentCategory`;
-    END IF;
+    ALTER TABLE `smartstone_categories` RENAME COLUMN `CategoryType` TO `ParentCategory`;
+END IF;
 END //
 DELIMITER ; -- 
 CALL RenameCategoryTypeIfExists();
 DROP PROCEDURE RenameCategoryTypeIfExists;
 
-ALTER TABLE `smartstone_services` ADD COLUMN IF NOT EXISTS `Category` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ServiceId`;
-ALTER TABLE `smartstone_pets` ADD COLUMN IF NOT EXISTS `Type` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `CreatureId`;
-ALTER TABLE `smartstone_costumes` ADD COLUMN IF NOT EXISTS `Scale` FLOAT NOT NULL DEFAULT 1 AFTER `DisplayId`;
+DELIMITER //
+CREATE PROCEDURE AddCategoryToSmartstoneServicesIfNotExists()
+BEGIN -- ALTER TABLE `smartstone_services` ADD COLUMN `Category` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ServiceId`;
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'smartstone_services' AND COLUMN_NAME = 'Category') THEN
+    ALTER TABLE `smartstone_services` ADD COLUMN `Category` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ServiceId`;
+END IF;
+END //
+DELIMITER ;
+CALL AddCategoryToSmartstoneServicesIfNotExists();
+DROP PROCEDURE AddCategoryToSmartstoneServicesIfNotExists;
+
+DELIMITER // 
+CREATE PROCEDURE AddTypeToSmartstonePetsIfNotExists()
+BEGIN -- ALTER TABLE `smartstone_pets` ADD COLUMN `Type` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `CreatureId`;
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'smartstone_pets' AND COLUMN_NAME = 'Type') THEN
+    ALTER TABLE `smartstone_pets` ADD COLUMN `Type` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `CreatureId`;
+END IF;
+END //
+DELIMITER ;
+CALL AddTypeToSmartstonePetsIfNotExists();
+DROP PROCEDURE AddTypeToSmartstonePetsIfNotExists;
+
+DELIMITER // -- ALTER TABLE `smartstone_costumes` ADD COLUMN `Scale` FLOAT NOT NULL DEFAULT 1 AFTER `DisplayId`;
+CREATE PROCEDURE AddScaleToSmartstoneCostumesIfNotExists()
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'smartstone_costumes' AND COLUMN_NAME = 'Scale') THEN
+    ALTER TABLE `smartstone_costumes` ADD COLUMN `Scale` FLOAT NOT NULL DEFAULT 1 AFTER `DisplayId`;
+END IF;
+END //
+DELIMITER ;
+CALL AddScaleToSmartstoneCostumesIfNotExists();
+DROP PROCEDURE AddScaleToSmartstoneCostumesIfNotExists;
