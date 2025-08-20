7eda229119fc23fce484f9d00c7ebcf380b7430e
Ryan Turner <AugustoMendes4426@gmail.com>
2025-08-20 14:06:15 +0100

    Update 1_sts_update_table_structure.sql

--- data/sql/db-world/updates/1_sts_update_table_structure.sql
+++ data/sql/db-world/updates/1_sts_update_table_structure.sql
@@ -1,4 +1,5 @@
-ALTER TABLE `smartstone_categories` RENAME COLUMN `CategoryType` TO `ParentCategory`;
-ALTER TABLE `smartstone_services` ADD COLUMN `Category` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ServiceId`;
-ALTER TABLE `smartstone_pets` ADD COLUMN `Type` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `CreatureId`;
-ALTER TABLE `smartstone_costumes` ADD COLUMN `Scale` FLOAT NOT NULL DEFAULT 1 AFTER `DisplayId`;
+-- ADD always "IF NOT EXISTS" between ADD COLUMN and Table name
+ALTER TABLE `smartstone_categories` RENAME COLUMN IF NOT EXISTS `CategoryType` TO `ParentCategory`;
+ALTER TABLE `smartstone_services` ADD COLUMN IF NOT EXISTS `Category` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ServiceId`;
+ALTER TABLE `smartstone_pets` ADD COLUMN IF NOT EXISTS `Type` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `CreatureId`;
+ALTER TABLE `smartstone_costumes` ADD COLUMN IF NOT EXISTS `Scale` FLOAT NOT NULL DEFAULT 1 AFTER `DisplayId`;
